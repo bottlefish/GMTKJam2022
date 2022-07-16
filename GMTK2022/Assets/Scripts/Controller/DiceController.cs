40cf3b3b3c5f26212b3f2be2e22bba2605642ef2
Ding Yuntian <1491671119@qq.com>
2022-07-16 22:28:15 +0800

    骰子弹匣和回收骰子逻辑实现

--- GMTK2022/Assets/Scripts/Controller/DiceController.cs
+++ GMTK2022/Assets/Scripts/Controller/DiceController.cs
@@ -8,8 +8,8 @@ public class DiceController : MonoBehaviour
     // Start is called before the first frame update
 
     // Update is called once per frame
-    public float ForceAmount=10000f;
-    public float TorqueAmount=20f;
+    public float ForceAmount = 10000f;
+    public float TorqueAmount = 20f;
 
     /*public enum State{
         one,
@@ -22,16 +22,25 @@ public class DiceController : MonoBehaviour
 
     //public State state;
 
-    private int state=5;
+    private int state = 5;
+
+    public int State
+    {
+        get
+        {
+            return state;
+        }
+    }
+
     //
     void Start()
     {
-       //RandomDice();
-       SetDiceFace();
+
     }
+
     void Update()
     {
-        
+
     }
 
     /*void RandomDice()
@@ -70,32 +79,31 @@ public class DiceController : MonoBehaviour
 
     void SetDiceFace()
     {
-         state=Random.Range(1,6);
-        if(state==1)
+        if (state == 1)
         {
             transform.eulerAngles = new Vector3(0f, 0, 0f);
         }
-        if(state==2)
+        if (state == 2)
         {
-           // transform.DORotate(new Vector3(0,0,90),0.1f);
+            // transform.DORotate(new Vector3(0,0,90),0.1f);
             transform.eulerAngles = new Vector3(0f, 0, 90f);
         }
-        if(state==3)
+        if (state == 3)
         {
             //transform.DORotate(new Vector3(180,0,0),0.1f);
             transform.eulerAngles = new Vector3(180f, 0, 0f);
         }
-        if(state==4)
+        if (state == 4)
         {
             //transform.DORotate(new Vector3(90,0,0),0.1f);
             transform.eulerAngles = new Vector3(90f, 0, 0f);
         }
-        if(state==5)
+        if (state == 5)
         {
             //transform.DORotate(new Vector3(0,0,270),0.1f);
             transform.eulerAngles = new Vector3(0, 0, 270f);
         }
-        if(state==6)
+        if (state == 6)
         {
             //transform.DORotate(new Vector3(180,0,0),0.1f);
             transform.eulerAngles = new Vector3(180, 0, 0);
@@ -105,30 +113,53 @@ public class DiceController : MonoBehaviour
             //transform.DORotate(Vector3.zero,0.1f);
         }
         Debug.Log(state);
-        
-        
     }
-    private void OnCollisionEnter(Collision other) {
-        if(other.transform.tag=="Enemy")
+
+    private void RandomDiceFace()
+    {
+        state = Random.Range(1, 7);
+        SetDiceFace();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.tag == "Enemy")
         {
-            
-
-
             Destroy(other.gameObject);
-            transform.GetComponent<Rigidbody>().freezeRotation=false;
+            transform.GetComponent<Rigidbody>().freezeRotation = false;
             //transform.DOJump(new Vector3(transform.position.x,transform.position.y+5,transform.position.z),2,1,0.5f,true);
-            transform.DOLocalRotate(new Vector3(30,30,30),0.02f).SetLoops(24,LoopType.Incremental).OnComplete(()=>{SetDiceFace();});                                                
+            transform.DOLocalRotate(new Vector3(30, 30, 30), 0.02f).SetLoops(24, LoopType.Incremental).OnComplete(() => { RandomDiceFace(); });
             //transform.GetComponent<Rigidbody>().AddTorque(transform.up*TorqueAmount,ForceMode.Impulse);
             //transform.GetComponent<Rigidbody>().AddForce(new Vector3(0,1,0)*ForceAmount);
-            transform.DOLocalMoveX(transform.position.x+Random.Range(-2f,2f),0.8f).SetEase(Ease.OutBack);
-            transform.DOLocalMoveZ(transform.position.z+Random.Range(-2f,2f),0.8f).SetEase(Ease.OutBack);
-            transform.DOLocalMoveY(5,0.8f).SetEase(Ease.OutBack).OnComplete(()=>{
-            transform.DOLocalMoveY(0,0.3f).SetEase(Ease.OutBack).OnComplete(()=>{              
-                transform.GetComponent<Rigidbody>().freezeRotation=true;
-                //transform.GetComponent<Rigidbody>().isKinematic=true;            
-                    });}
+            transform.DOLocalMoveX(transform.position.x + Random.Range(-2f, 2f), 0.8f).SetEase(Ease.OutBack);
+            transform.DOLocalMoveZ(transform.position.z + Random.Range(-2f, 2f), 0.8f).SetEase(Ease.OutBack);
+            transform.DOLocalMoveY(5, 0.8f).SetEase(Ease.OutBack).OnComplete(() =>
+                {
+                    transform.DOLocalMoveY(0, 0.3f).SetEase(Ease.OutBack).OnComplete(() =>
+                    {
+                        transform.GetComponent<Rigidbody>().freezeRotation = true;
+                        transform.GetComponent<Rigidbody>().isKinematic = true;
+                    });
+                }
             );
-                  
         }
+    }
+
+    /// <summary>
+    /// 初始化骰子状态并旋转
+    /// </summary>
+    /// <param name="diceState">骰子状态</param>
+    public void InitDiceState(int diceState)
+    {
+        state = diceState;
+        SetDiceFace();
+    }
+
+    /// <summary>
+    /// 销毁自身
+    /// </summary>
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
     }
 }
--- GMTK2022/Assets/Scripts/Controller/GunController.cs
+++ GMTK2022/Assets/Scripts/Controller/GunController.cs
@@ -10,23 +10,81 @@ public class GunController : MonoBehaviour
 
     public float timeBetweenShots;
     public float shotCounter;
+    public int diceBoxSize = 6;
+
+    private Queue<int> diceQueue;
 
     public Transform firePoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitDiceQueue();
+    }
+
+    // 初始化骰子队列，随机
+    private void InitDiceQueue()
+    {
+        diceQueue = new Queue<int>();
+        for (int i = 0; i < diceBoxSize; i++)
+        {
+            diceQueue.Enqueue(Random.Range(1, 7));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            DiceController newDice=Instantiate(dice,firePoint.position,Quaternion.identity) as DiceController;
-                newDice.GetComponent<Rigidbody>().freezeRotation=true;
-                newDice.GetComponent<Rigidbody>().AddForce(transform.forward*ForceAmount,ForceMode.Impulse);
-                
+            if (diceQueue.Count == 0)
+            {
+                // 手上没有骰子了
+                Debug.Log("手上没有骰子！");
+                return;
+            }
+            else
+            {
+                // 手上有骰子，发射并从骰子弹匣出栈
+                ShootDice(diceQueue.Dequeue());
+            }
+        }
+    }
+
+    private void ShootDice(int diceState)
+    {
+        Debug.Log("点数：" + diceState);
+        DiceController newDice = Instantiate(dice, firePoint.position, Quaternion.identity) as DiceController;
+        // 为新骰子设置点数
+        newDice.InitDiceState(diceState);
+        newDice.GetComponent<Rigidbody>().AddForce(transform.forward * ForceAmount, ForceMode.Impulse);
+        newDice.GetComponent<Rigidbody>().freezeRotation = true;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        DiceController[] colliderDiceArray = other.GetComponents<DiceController>();
+        
+        if (colliderDiceArray.Length != 0)
+        {
+            foreach (var colliderDice in colliderDiceArray)
+            {
+                RecycleDice(colliderDice);
+            }
+        }
+    }
+
+    // 回收骰子装填弹匣
+    private void RecycleDice(DiceController colliderDice)
+    {
+        if (diceQueue.Count == diceBoxSize)
+        {
+            Debug.Log("骰子已满");
+            return;
+        }
+        else
+        {
+            // 骰子进入弹匣并销毁骰子
+            diceQueue.Enqueue(colliderDice.State);
+            colliderDice.DestroySelf();
         }
     }
 }
